f55b3d845408e74acfe2aaf1f0af2821e82c3aca
midhilesh <chintu.midhilesh@gmail.com>
2026-09-05 00:59:33 +0530

    Add C++ competitive programming template

---
+++ cp_template.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+using vi = vector<int>;
+using vll = vector<ll>;
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+
+const ll INF = (ll)4e18;
+const int MOD = 998244353;
+const int INV2 = (MOD + 1) / 2;
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define sz(v) ((int)(v).size())
+
+#define pb push_back
+#define eb emplace_back
+
+#define fi first
+#define se second
+
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rrep(i, a, b) for (int i = (a); i >= (b); i--)
+
+void solve() {
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
+    return 0;
+}
